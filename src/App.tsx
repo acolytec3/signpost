023d46afa8ac490914c324a9520d04f5b2a3a60d
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2021-03-14 20:15:46 -0400

    Add more error catching

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import {
   Button,
   Center,
   VStack,
+  useToast,
 } from "@chakra-ui/react";
 import Ipfs from "ipfs";
 import FileUploader from "./components/fileUpload";
@@ -15,6 +16,7 @@ let web3: any;
 
 function App() {
   const [state, dispatch] = React.useReducer(reducer, initialState);
+  const toast = useToast();
 
   const startUp = async () => {
     try {
@@ -37,21 +39,41 @@ function App() {
     networkId: 42,
     subscriptions: {
       wallet: (wallet) => {
-        //@ts-ignore
-        window.ethereum.enable()
-        web3 = new ethers.providers.Web3Provider(wallet.provider);
-        dispatch({ type: "SET_WEB3", payload: { web3: web3 } });
+        try {
+          //@ts-ignore
+          window.ethereum.enable();
+          web3 = new ethers.providers.Web3Provider(wallet.provider);
+          dispatch({ type: "SET_WEB3", payload: { web3: web3 } });
+        } catch (err) {
+          console.log(err);
+          toast({
+            position: "top",
+            status: "error",
+            title: "Something went wrong",
+            description: err.toString(),
+            duration: 5000,
+          });
+        }
       },
-
     },
   });
 
   const handleConnect = async () => {
-
-    const walletSelected = await onboard.walletSelect()
-//    const read = await onboard.walletCheck()
-//    console.log(read);
-  }
+    try {
+      const walletSelected = await onboard.walletSelect();
+    } catch (err) {
+      console.log(err);
+      toast({
+        position: "top",
+        status: "error",
+        title: "Something went wrong",
+        description: err.toString(),
+        duration: 5000,
+      });
+    }
+    //    const read = await onboard.walletCheck()
+    //    console.log(read);
+  };
   return (
     <ChakraProvider>
       <GlobalContext.Provider value={{ dispatch, state }}>
